382c9c39616cc6bf6554509d883523099609a678
Adnan Smajic <adnan.smajic4431@hotmail.com>
2026-01-12 15:39:36 +0100

    users controller

--- src/main/java/com/car_rental/car_rental/controller/UserController.java
+++ src/main/java/com/car_rental/car_rental/controller/UserController.java
@@ -4,10 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,22 +29,21 @@ public class UserController {
   private final UserMapper userMapper;
 
   @PostMapping("/manager")
-  public ResponseEntity<String> createManagerUser(RegisterRequest request) {
-    System.out.println("-------------____________________----------" + request);
-    // userService.saveManagerUser(request);
+  @PreAuthorize("hasRole('ADMIN')")
+  public ResponseEntity<String> createManagerUser(@RequestBody RegisterRequest request) {
+    userService.saveManagerUser(request);
     return ResponseEntity.ok("Manager user created successfully!");
   }
 
-  @PostMapping("/admin/test/test")
-  public ResponseEntity<String> createAdminUser(User user) {
-    System.out.println("-------------____________________----------" + user);
-    // System.out.println("-------------____________________----------" + user.getPassword());
-    // System.out.println("-------------____________________----------" + user.getUsername());
-    // userService.saveAdminUser(request);
+  @PostMapping("/admin")
+  @PreAuthorize("hasRole('ADMIN')")
+  public ResponseEntity<String> createAdminUser(@RequestBody RegisterRequest request) {
+    userService.saveAdminUser(request);
     return ResponseEntity.ok("Admin user created successfully!");
   }
 
   @GetMapping
+  @PreAuthorize("hasRole('ADMIN')")
   public ResponseEntity<List<UserDto>> getAllUsers() {
     List<User> users = userService.getAllUsers();
     return ResponseEntity.ok(users.stream().map(userMapper::toDto).toList());
@@ -60,6 +61,7 @@ public class UserController {
   }
 
   @DeleteMapping("/{id}")
+  @PreAuthorize("hasRole('ADMIN')")
   public ResponseEntity<Void> deleteUser(@PathVariable(name = "id") Long id) {
     Optional<User> user = userService.getUser(id);
 
