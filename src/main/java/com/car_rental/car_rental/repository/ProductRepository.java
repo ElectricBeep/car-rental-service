3a577b8f5bc01df3305a21e0c5dc9bf19269db04
Adnan Smajic <adnan.smajic4431@hotmail.com>
2026-01-12 15:41:06 +0100

    removed product model

--- src/main/java/com/car_rental/car_rental/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.car_rental.car_rental.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.car_rental.car_rental.model.Product;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-
-}
