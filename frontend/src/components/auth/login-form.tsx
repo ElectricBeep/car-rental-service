76be271703bfc58e0d5c54e0ffdfc7744c4239c9
Adnan Smajic <adnan.smajic4431@hotmail.com>
2026-02-27 23:29:21 +0100

    login form improvments

--- frontend/src/components/auth/login-form.tsx
+++ frontend/src/components/auth/login-form.tsx
@@ -1,86 +1,116 @@
 "use client";
 
-import { useState } from "react";
-import { useRouter, useSearchParams } from "next/navigation";
-import Link from "next/link";
 import { useForm } from "react-hook-form";
+import { useState } from "react";
+import Link from "next/link";
 import * as z from "zod";
-import { toast } from "sonner"
+import { toast } from "sonner";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { signIn } from "next-auth/react";
 
 import { CardWrapper } from "./card-wrapper";
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { FormError } from "@/components/form-error";
 import { loginFormSchema } from "@/lib/validations";
-import { signIn, useSession } from "next-auth/react";
-import { TextField } from "../forms/text-field";
-import { SubmitField } from "../forms/submit-field";
-
-type LoginFormSchema = z.infer<typeof loginFormSchema>
 
 export const LoginForm = () => {
-  const search = useSearchParams()
+  const [isLoading, setIsLoading] = useState(false);
 
-  const { register, handleSubmit, formState } = useForm<LoginFormSchema>({
-    resolver: zodResolver(loginFormSchema)
-  })
+  const form = useForm<z.infer<typeof loginFormSchema>>({
+    resolver: zodResolver(loginFormSchema),
+    defaultValues: {
+      username: "",
+      password: "",
+    },
+  });
 
-  const onSubmitHandler = handleSubmit((data) => {
-    signIn("credentials", {
-      username: data.username,
-      password: data.password,
-      callbackUrl: "/dashboard"
-    })
-  })
+  const onSubmit = async (values: z.infer<typeof loginFormSchema>) => {
+    try {
+      setIsLoading(true);
+
+      const result = await signIn("credentials", {
+        username: values.username,
+        password: values.password,
+        redirect: false,
+        callbackUrl: "/dashboard"
+      });
+
+      if (result?.error) {
+        toast("Invalid username or password!");
+      }
+    } catch (error: any) {
+      toast("Something went wrong");
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   return (
     <CardWrapper headerLabel="Welcome back">
-      <form
-        onSubmit={onSubmitHandler}
-        action="/api/auth/callback/credentials"
-        className="space-y-6"
-      >
-        <div className="space-y-4">
-          <TextField
-            type="text"
-            register={register("username")}
-            formState={formState}
-            label="Username"
-            placeholder="Email address or username"
-          />
-
-          <TextField
-            type="password"
-            register={register("password", { required: true })}
-            formState={formState}
-            label="Password"
-            placeholder="Enter your password"
-          />
-        </div>
-        {/* <FormError message={error} /> */}
-        {/* <div className="flex justify-center">
-            <Button
-              size="sm"
-              variant="link"
-              asChild
-              className="px-0 font-normal"
-            >
-              <Link href="/auth/reset">
-                Forgot password?
-              </Link>
-            </Button>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-6"
+        >
+          <div className="space-y-4">
+            <FormField
+              control={form.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Username</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      placeholder="Username"
+                      type="text"
+                      disabled={isLoading}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      placeholder="********"
+                      type="password"
+                      disabled={isLoading}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                  <Button
+                    size="sm"
+                    variant="link"
+                    asChild
+                    className="px-0 font-normal"
+                  >
+                    <Link href="/auth/reset">
+                      Forgot password?
+                    </Link>
+                  </Button>
+                </FormItem>
+              )}
+            />
           </div>
           <Button
             type="submit"
-            className="w-full bg-violet-500 hover:bg-violet-400 text-white"
+            className="w-full bg-violet-500 hover:bg-violet-400 text-white cursor-pointer"
             disabled={isLoading}
           >
             Login
-          </Button> */}
-        <SubmitField>Sign in</SubmitField>
-      </form>
+          </Button>
+        </form>
+      </Form>
     </CardWrapper>
   );
 };
+
